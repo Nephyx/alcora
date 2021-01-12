598a560b6f0375edf57c558fdcc511ec5c472afe
Maroš Kováč <maros1305@gmail.com>
2021-01-12 21:29:46 +0100

    Display count of files and dirs within the path

--- alcora/main.py
+++ alcora/main.py
@@ -11,17 +11,27 @@ def main(directory_path):
         dir_contents = listdir(dir_path)
 
         if len(dir_contents) > 0:
-            print("Items in directory: " + str(len(dir_contents)))
+            file_count = 0
+            dir_count = 0
 
             for file in dir_contents:
                 file_path = dir_path + "/" + file
                 file_size = path.getsize(file_path)
                 modification_datetime = datetime.fromtimestamp(path.getmtime(file_path))
 
+                if path.isdir(file_path):
+                    dir_count += 1
+                elif path.isfile(file_path):
+                    file_count += 1
+
                 print(file + " (" + str(file_size) + " bytes)", end="")
                 print(" [directory]" if path.isdir(file_path) else " [file]")
 
                 print("   Modified: " + str(modification_datetime))
+            
+            print(('Items in directory: {items} '
+                '({dirs} directories, {files} files)') \
+                .format(items=str(len(dir_contents)), dirs=str(dir_count), files=str(file_count)))
     else:
         print("Specified path " + dir_path + " is not a directory.")
 
